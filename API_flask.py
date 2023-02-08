0d9df492dbef9ad3ffe7302df6619cacda57a366
NordineOub <75072085+NordineOub@users.noreply.github.com>
2023-02-08 11:52:55 +0100

    Add prometheus metrics

--- API_flask.py
+++ API_flask.py
@@ -3,11 +3,33 @@ import joblib
 import numpy as np
 import json
 
+# Metrique
+from prometheus_client import (
+    Counter,
+    generate_latest,
+    Histogram,
+    REGISTRY,
+)
+PYTHON_REQUESTS_COUNTER = Counter("python_requests", "total requests")
+PYTHON_FAILED_REQUESTS_COUNTER = Counter("python_failed_requests", "failed requests")
+PYTHON_LATENCIES_HISTOGRAM = Histogram(
+    "python_request_latency", "request latency by path"
+)
+
+
+
+
+#Importation du modele
 knn =joblib.load('knn_Iris.pkl')
 
 
 app = Flask(__name__)
 
+@app.route("/metrics", methods=["GET"])
+def stats():
+    return generate_latest(REGISTRY), 200
+
+@PYTHON_LATENCIES_HISTOGRAM.time()
 @app.route('/predict', methods=['POST'])
 def predict():
     # Get the data from the POST request.
@@ -32,5 +54,8 @@ def predict():
 
 
 
+
+
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
+
